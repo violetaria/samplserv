010a9fc29353fc031d93dce2e628b4f7b24caea8
Brit Butler <redline6561@gmail.com>
2015-10-20 14:38:22 -0400

    Add some basic routes.

--- lib/samplserv.rb
+++ lib/samplserv.rb
@@ -1,5 +1,26 @@
 require "samplserv/version"
+require "sinatra/base"
+require "pry"
 
 module Samplserv
-  # Your code goes here...
+  class App < Sinatra::Base
+    set :logging, true
+
+    get "/" do
+      "Welcome to Samplserv!"
+    end
+
+    post "/better" do
+      version = params["v"] ? params["v"].to_i : 1
+      # binding.pry
+      spawn("afplay \"samples/Better#{version}.mp3\"")
+    end
+
+    get "/stop" do
+      spawn("killall afplay")
+      "So much for the music..."
+    end
+
+    run! if app_file == $0
+  end
 end
